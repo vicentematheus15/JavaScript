6dd5592c81a9c74b86edd9e6e8741b6556f8bd0e
Matheus Vicente <vicentematheus1507@gmail.com>
2025-06-16 17:30:10 -0300

    feat: update HTML structure and implement tabuada calculation function

---
+++ aula14 - Site04/script.js
@@ -0,0 +1,7 @@
+function calcularTabuada(){
+    let num  = document.getElementById("num").value
+
+    if(num == ""){
+        alert("Digite um número válido para calcular a sua tabuada")
+    }
+}
