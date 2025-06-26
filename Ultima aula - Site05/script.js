58560c96eb311b8b15d904225a6d1d6a30df8f63
Matheus Vicente <vicentematheus1507@gmail.com>
2025-06-26 15:38:26 -0300

    refactor: enhance comments for clarity and improve code readability in script.js

--- Ultima aula - Site05/script.js
+++ Ultima aula - Site05/script.js
@@ -4,6 +4,7 @@ let res = document.getElementById("res")
 let valores = []
 
 function isNumero(n){
+    //se o valor numerico do numero for entre 1 e 100, retorna true. Qualquer outro valor indesejado retorna false
     if(Number(n) >= 1 && Number(n) <= 100){
         return true
     }else{
@@ -12,6 +13,8 @@ function isNumero(n){
 }
 
 function inLista(n, l){
+    //o metodo indexOf, busca na lista "l" pelo valor "n" e retorna o índice em que achou esse valor. se não achar o valor na array, retorna -1.
+    //nessa linha, se o valor retornado for diferente de -1, significa que o item ja existe na array e retorna true na função. caos contrario, retorna false
     if(l.indexOf(n) != -1){
         return true
     }else {
@@ -21,18 +24,25 @@ function inLista(n, l){
 
 
 function adicionar(){
+    //se a funçao isNumero(pra saber se é um numero válido)for verdadeira E a função inLista(pra saber se o numero ja foi adicionado na lista) for falsa (a "!" no inLista inverte)
     if(isNumero(num.value) && !inLista(num.value, valores)){
-        valores.push(Number(num.value)) 
-        let item = document.createElement('option')
-        item.text = `O valor ${num.value} foi adicionado a lista;`
-        lista.appendChild(item)
+        valores.push(Number(num.value)) //adiciona o valor numerico de num na array valores
+        let item = document.createElement('option') //cria um elemento option para aparecer no select e uma variavel iem que recebe o option criado
+        item.text = `O valor ${num.value} foi adicionado;` //o texto do item que foi criado é mostrado no select
+        lista.appendChild(item) //adiciona na "lista" (variavel que foi criada no js pra receber o ID do select) o item criado
 
-        console.log(valores)
     }else{
         alert("Valor inválido ou já encontrado na lista!")
     }
+    num.value = "" //apaga do input o número que foi adicionado
+    num.focus() //após adicionar, é como se ja tivesse clicado novamente no input para adicionar um novo valor
 }
 
 function finalizar(){
-    res.innerHTML = `Ao todo, temos ${valores.length} cadastrados.`
+    if(valores.length == 0){
+        alert("Adicione valores antes de finalizar!")
+    }else{
+        res.innerHTML = ""
+        // `Ao todo, temos ${valores.length} cadastrados.`
+    }
 }
