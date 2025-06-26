ae208de3b76a9ab96b5f942892e70f54ff02b518
Matheus Vicente <vicentematheus1507@gmail.com>
2025-06-26 13:20:12 -0300

    refactor: reorganize HTML structure and update JavaScript functions for number validation

--- Ultima aula - Site05/script.js
+++ Ultima aula - Site05/script.js
@@ -1,12 +1,21 @@
 let num = document.getElementById("num").value
-function adicionar(){
-    
-    if(num == "" || num > 100 || num < 1){
-        alert("Digite um número válido para adicionar!")
-    }else{
-        let arrayNum = Number([num])
-        console.log(arrayNum)
-        arrayNum.push(num)
-    }
+let n = Number(num)
+let lista = document.getElementById("lista").value
+let res = document.getElementById("res").value
+valores = []
 
+function isNumero(n){
+    if(n < 1 || n > 100 || n == "")
+        return false
 }
+
+function inLista(n, valores){
+    if(valores.indexOf(n) != -1)
+        return false
+}
+
+function adicionar(){ 
+    if(isNumero(n) && inLista(n, valores) == false)
+        alert("Digite um número válido para adicionar!")
+}
+
